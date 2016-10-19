e296caf10b1a27507c2bff33ccd19a67cefa363a
sherri010 <aminshahidi.sh@gmail.com>
2016-10-19 10:31:10 -0700

    refactored

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,21 @@
 class User < ApplicationRecord
-   attr_reader :password
+   # attr_reader :password
    validates :password_digest,:email, presence: true
-   validates :password, confirmation: true
+   # validates :password, confirmation: true
+
+   has_secure_password
 
  def self.confirm(email, password)
      user = User.find_by(email: email)
      user.authenticate(password)
  end
- def password=(password)
-  @password = password
-  self.password_digest = BCrypt::Password.create(password)
-end
+#  def password=(password)
+#   @password = password
+#   self.password_digest = BCrypt::Password.create(password)
+# end
 
-def authenticate(password)
-   BCrypt::Password.new(self.password_digest) == password ? self:false
-end
+# def authenticate(password)
+#    BCrypt::Password.new(self.password_digest) == password ? self:false
+# end
 
 end
