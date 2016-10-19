2b3b7fb067bb462e1358e823188826c86c5ecc2b
sherri010 <aminshahidi.sh@gmail.com>
2016-10-19 10:28:49 -0700

    basics of auth

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+   attr_reader :password
+   validates :password_digest,:email, presence: true
+   validates :password, confirmation: true
+
+ def self.confirm(email, password)
+     user = User.find_by(email: email)
+     user.authenticate(password)
+ end
+ def password=(password)
+  @password = password
+  self.password_digest = BCrypt::Password.create(password)
+end
+
+def authenticate(password)
+   BCrypt::Password.new(self.password_digest) == password ? self:false
+end
+
+end
